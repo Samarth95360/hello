e3aa8a7f2950441dfa733811eb77d725d98253ed
Samarth95360 <77107933+Samarth95360@users.noreply.github.com>
2023-12-29 11:16:18 +0530

    Add files via upload

---
+++ Hello-world-Jenkins/src/test/java/com/example/HelloworldJenkins/HelloWorldJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloworldJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
